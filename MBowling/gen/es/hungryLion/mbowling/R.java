4e23d40bd59aacde77c1c5ff81211f72610e0f01
fefete <metrack92@gmail.com>
2012-11-07 18:39:11 +0100

    Aplicar la clase "ClasificacionGlobal" donde se encuentran filtros y clasificacion de bolera

--- MBowling/gen/es/hungryLion/mbowling/R.java
+++ MBowling/gen/es/hungryLion/mbowling/R.java
@@ -24,40 +24,78 @@ public final class R {
         public static final int ranking1=0x7f02000a;
     }
     public static final class id {
-        public static final int Listview=0x7f060003;
-        public static final int RelativeLayout1=0x7f060004;
-        public static final int Reservadas=0x7f060001;
-        public static final int Reservar=0x7f060002;
-        public static final int TODAS=0x7f060000;
-        public static final int bSalir=0x7f06000c;
-        public static final int btnlogin=0x7f06000d;
-        public static final int button1=0x7f060011;
-        public static final int clasifImg=0x7f060006;
-        public static final int datePicker1=0x7f06000f;
-        public static final int editText1=0x7f06000a;
-        public static final int editText2=0x7f06000b;
-        public static final int imageView1=0x7f060012;
-        public static final int listView1=0x7f060009;
-        public static final int logoutImg=0x7f060007;
-        public static final int pistasImg=0x7f060008;
-        public static final int rankingImg=0x7f060005;
-        public static final int textView1=0x7f06000e;
-        public static final int textView2=0x7f060010;
+        public static final int AGE=0x7f060000;
+        public static final int Listview=0x7f06000b;
+        public static final int RelativeLayout1=0x7f06000c;
+        public static final int Reservadas=0x7f060009;
+        public static final int Reservar=0x7f06000a;
+        public static final int TODAS=0x7f060008;
+        public static final int a=0x7f06000e;
+        public static final int bSalir=0x7f060024;
+        public static final int back=0x7f060012;
+        public static final int botonname=0x7f060017;
+        public static final int btnlogin=0x7f060025;
+        public static final int button1=0x7f060002;
+        public static final int clasifImg=0x7f06001e;
+        public static final int datePicker1=0x7f060026;
+        public static final int de=0x7f060010;
+        public static final int editText1=0x7f060022;
+        public static final int editText2=0x7f060023;
+        public static final int female=0x7f060018;
+        public static final int findage=0x7f060011;
+        public static final int findsex=0x7f06001c;
+        public static final int imageView1=0x7f060028;
+        public static final int intervalo1=0x7f06000f;
+        public static final int intervalo2=0x7f06000d;
+        public static final int listView1=0x7f060021;
+        public static final int logoutImg=0x7f06001f;
+        public static final int male=0x7f06001b;
+        public static final int pistasImg=0x7f060020;
+        public static final int radioage=0x7f060004;
+        public static final int radioall=0x7f060007;
+        public static final int radiofiltro=0x7f060003;
+        public static final int radiogsex=0x7f06001a;
+        public static final int radioname=0x7f060005;
+        public static final int radiosex=0x7f060006;
+        public static final int rankingImg=0x7f06001d;
+        public static final int searchname=0x7f060013;
+        public static final int searchsurname=0x7f060015;
+        public static final int sextext=0x7f060019;
+        public static final int textView1=0x7f060001;
+        public static final int textView2=0x7f060027;
+        public static final int textname=0x7f060014;
+        public static final int textsurname=0x7f060016;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int layouttransicion=0x7f030001;
-        public static final int listalibres=0x7f030002;
-        public static final int login=0x7f030003;
-        public static final int reserva=0x7f030004;
-        public static final int splash=0x7f030005;
+        public static final int activity_clasificacion_global=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int buscaage=0x7f030002;
+        public static final int buscaname=0x7f030003;
+        public static final int buscasex=0x7f030004;
+        public static final int layouttransicion=0x7f030005;
+        public static final int listalibres=0x7f030006;
+        public static final int login=0x7f030007;
+        public static final int reserva=0x7f030008;
+        public static final int splash=0x7f030009;
     }
     public static final class string {
         public static final int Login=0x7f040001;
+        public static final int NombreApellido=0x7f040009;
+        public static final int RadioAge=0x7f04000b;
+        public static final int RadioName=0x7f04000c;
+        public static final int RadioSexo=0x7f04000a;
         public static final int Reservar=0x7f040002;
         public static final int Salir=0x7f040006;
         public static final int Siguiente=0x7f040005;
+        public static final int agebetween=0x7f04000f;
         public static final int app_name=0x7f040000;
+        public static final int back=0x7f040010;
+        public static final int filterby=0x7f04000e;
+        public static final int generalranking=0x7f04000d;
+        public static final int textname=0x7f040008;
+        /** MODIFICACIONES F.FERRANDO PARA CLASE "CLASIFICACIONGLOBAL" 
+         */
+        public static final int title_activity_clasificacion_global=0x7f040007;
         public static final int txtDia=0x7f040003;
         public static final int txtPersonas=0x7f040004;
     }
